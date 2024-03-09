0108c53856ed06c78ab673bad3b6abcb04d0b9b0
Shayak Barh <shayakbarh10@gmail.com>
2024-03-10 00:23:18 +0530

    Finding the eigenvalues using QR decomposition

---
+++ qr_decomp.py
@@ -0,0 +1,21 @@
+# QR decomposition
+import numpy as np
+
+# making the A matrix
+A = np.array([[5,-2],[-2,8]])
+
+# Q, R decomposition using numpy.linalg.qr
+Q, R = np.linalg.qr(A) 
+
+# print the Q and R matrix
+print("The Q matrix: \n", Q, "\n The R matix: \n",R)
+
+
+# calculate eigenvalues using the decomposition
+eigenval_qr = np.diag(R)
+print("Eigenvalues of A using QR decomposition: ", eigenval_qr)
+
+
+# Eigenvalues and eigenvectors produced by numpy.linalg.eigh
+eigenval_eigh, eigenvec_eigh = np.linalg.eigh(A)
+print("Eigenvalues of A using np.linalg.eigh: ", eigenval_eigh)
